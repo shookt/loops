23750f935f71d07e2f29c8ec34ddc2221b287e29
shookt <tshook428@gmail.com>
2016-02-22 11:32:39 -0500

    vowel

---
+++ GetVowel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("enter vowel");
+            input = Console.ReadLine();
+            while(response == "a"/"e"/"i"/"o"/"u"/"A"/"E"/"I"/"O"/"U")
+        }
+    }
+}
