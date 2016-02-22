a7592fab73e4b94af4d8c14af3d44cd321acdc86
shookt <tshook428@gmail.com>
2016-02-22 11:02:39 -0500

    lkjhgfds

---
+++ sum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int response, total;
+            total = 0;
+            Console.WriteLine("enter number");
+            input = Console.ReadLine();
+            response = Convert.ToInt32(input);
+            while (response < 999)
+            {
+                total += response;
+                Console.WriteLine("enter number");
+                input = Console.ReadLine();
+                response = Convert.ToInt32(input);
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
