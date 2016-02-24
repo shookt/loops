777b2499c9a7ac0b70b17a2c9541f6e63bf1fa2a
shookt <tshook428@gmail.com>
2016-02-24 11:33:38 -0500

    looooooooop

---
+++ MultiplicationTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                Console.Write("Row #" + y);
+                for (int x = 0; x < 10; x++)
+                {
+                    Console.Write(x + " ");
+                }
+            }
+        }
+    }
+}
