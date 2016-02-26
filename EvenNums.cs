bcae963e6a7b4a3aeeeb69ea6b84c0186b2b0a57
shookt <tshook428@gmail.com>
2016-02-26 11:22:09 -0500

    looooooo

---
+++ EvenNums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int e = 2; e <= 100; e = e + 2)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
