364a6ad54ffc4a385e07343aab492e32dda830b0
shookt <tshook428@gmail.com>
2016-02-26 11:30:00 -0500

    loooops

---
+++ TableOfSquares.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 20; n++)
+            {
+                Console.WriteLine(" The int value is {0} and the squared value is {1}.", n, n * n);
+            }
+        }
+    }
+}
